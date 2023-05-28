b207d92d7e50557754d40c0a424fd7508056652e
sedpro <sedpro@ya.ru>
2023-05-28 19:22:31 +0300

    tooltip hover overlapping thumbnails fixed

--- src/components/Tooltip.vue
+++ src/components/Tooltip.vue
@@ -1,19 +1,22 @@
 <template>
-    <div class="tooltip-container">
-        <slot />
-        <div class="tooltip">
+    <div class="tooltip-container" @mouseenter="enter()" @mouseleave="leave()">
+        <slot  />
+        <div class="tooltip" v-if="hover">
             <span class="text">{{ text }}</span>
         </div>
     </div>
   </template>
 
 <script setup lang="ts">
-import { toRefs } from 'vue';
+import { ref, toRefs } from 'vue';
 
 const props = defineProps<{
     text: string,
 }>()
 const { text } = toRefs(props);
+const hover = ref(false);
+const enter = () => hover.value = true;
+const leave = () => hover.value = false;
 </script>
 
 <style scoped>
@@ -21,26 +24,28 @@ const { text } = toRefs(props);
   position: relative;
 }
 
-.tooltip-container:hover .tooltip{
-  opacity: 1;
-}
-
 .tooltip { 
   color: #ffffff;
   text-align: center;
   border-radius: 3px;
+  padding: 0 2px;
 
   width: 200px;
   top: 100%;
   left: 50%;
   margin-left: -100px;
 
-  opacity: 0;
-  transition: opacity 1s;
-
   position: absolute;
   z-index: 1;
 
   background: rgba(0, 0, 0, 0.7);
+}
+.tooltip::after {
+  content: " ";
+  position: absolute;
+  bottom: 100%;
+  left: 50%;
+  border: 5px solid transparent;
+  border-bottom: 5px solid rgba(0, 0, 0, 0.7);
 }
 </style>
